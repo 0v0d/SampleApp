601ef445a0bea70be64102050e41dd73f603ee44
0v0d <legolesia@gmail.com>
2024-10-16 13:26:03 +0900

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 0v0 on 2024/10/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by 0v0 on 2024/10/16.
+//
+
+import Testing
+@testable import SampleApp
+
+struct SampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
